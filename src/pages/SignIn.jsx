5b67c458d2e801c5e3b5ef909e3bc652477432d7
Victor-Okpukpan <vokpukpan@gmail.com>
2023-02-17 15:58:19 +0100

    created the sign-in page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function SignIn() {
   return (
-    <div>
-      Sign In
-    </div>
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="key" className="w-full rounded-2xl" />
+        </div>
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form>
+            <input className="w-full" type="email" />
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
