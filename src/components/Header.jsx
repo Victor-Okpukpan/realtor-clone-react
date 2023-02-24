86d4d1e888d66e06c817863a144b39df2fda2b00
Victor-Okpukpan <vokpukpan@gmail.com>
2023-02-24 07:04:15 +0100

    completed the header component and made it dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-
+import {getAuth, onAuthStateChanged} from 'firebase/auth';
 export default function Header() {
+    const [pageState, setPageState] = useState("Sign In");
     const location = useLocation();
     const navigate = useNavigate();
+    const auth = getAuth();
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) =>{
+            if(user){
+                setPageState("Profile")
+            }else{
+                setPageState("Sign In")
+            }
+        })
+    }, [auth])
+    
 
     function pathMatchRoute(route){
         if(route === location.pathname){
@@ -21,7 +33,7 @@ export default function Header() {
                 <ul className="flex space-x-10">
                     <li className={`cursor-pointer py-3 text-sm font-semibold  border-b-[3px]  ${pathMatchRoute("/") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent"}`} onClick={()=>navigate("/")}>Home</li>
                     <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoute("/offers") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent"}`} onClick={()=>navigate("/offers")}>Offers</li>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoute("/sign-in") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent"}`} onClick={()=>navigate("/sign-in")}>Sign-In</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoute("/sign-in") || pathMatchRoute("/profile") ? "text-black border-b-red-500" : "text-gray-400 border-b-transparent"}`} onClick={()=>navigate("/sign-in")}>{pageState}</li>
                 </ul>
             </div>
         </header>
