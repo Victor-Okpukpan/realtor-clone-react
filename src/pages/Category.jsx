926aff20b654866c91625f9a9a3046fef7f45566
Victor-Okpukpan <vokpukpan@gmail.com>
2023-03-17 13:10:04 +0100

    fixed a minor bug

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -80,7 +80,7 @@ export default function Category() {
           )}
         </>
       ) : (
-        <p>There are no current offers</p>
+        <p>There are no current {params.categoryName === "rent" ? "places for rent" : "places for sale"}</p>
       )}
     </div>
   )
