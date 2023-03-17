82714434d3861989de7c428fe1221d33d1fd9783
Victor-Okpukpan <vokpukpan@gmail.com>
2023-03-17 14:27:11 +0100

    Added automatic close functionality to mobile navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,11 +41,11 @@ export default function Header() {
             <div className="sm:hidden flex flex-1 justify-end items-center py-3">
                 {toggle ? <MdClose onClick={() => setToggle(!toggle)} className="w-[28px] h-[28px] object-contain cursor-pointer transition duration-200 ease-in-out"/> : <MdMenu onClick={() => setToggle(!toggle)} className="w-[28px] h-[28px] object-contain cursor-pointer transition duration-200 ease-in-out" /> }
 
-                <div className={`${!toggle ? "hidden" : "flex"} p-6 bg-gradient-to-r from-gray-700 via-gray-900 to-black absolute top-12  right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl transition duration-200 ease-in-out`}>
+                <div className={`${!toggle ? "hidden" : "flex"} p-6 bg-gradient-to-r from-gray-600 via-gray-700 to-black absolute top-12  right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl transition duration-200 ease-in-out`}>
                 <ul className="flex justify-end items-start flex-col ">
-                    <li className={`cursor-pointer py-3 text-sm font-semibold  border-b-[3px]  ${pathMatchRoute("/") ? "text-black border-none" : "text-gray-400 border-b-transparent"}`} onClick={()=>navigate("/")}>Home</li>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoute("/offers") ? "text-black border-none" : "text-gray-400 border-b-transparent"}`} onClick={()=>navigate("/offers")}>Offers</li>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${(pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) ? "text-black border-none" : "text-gray-400 border-b-transparent"}`} onClick={()=>navigate("/profile")}>{pageState}</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold  border-b-[3px]  ${pathMatchRoute("/") ? "text-black border-none" : "text-gray-400 border-b-transparent"}`} onClick={()=>{navigate("/"); setToggle(false)}}>Home</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${pathMatchRoute("/offers") ? "text-black border-none" : "text-gray-400 border-b-transparent"}`} onClick={()=>{navigate("/offers"); setToggle(false)}}>Offers</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] ${(pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) ? "text-black border-none" : "text-gray-400 border-b-transparent"}`} onClick={()=>{navigate("/profile"); setToggle(false)}}>{pageState}</li>
                 </ul>
                 </div>
             </div>
